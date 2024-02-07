c472edf41011600a42a6096c448bf6703b706084
DESKTOP-LNFCMVA\HP <sixaruliketi@gmail.com>
2024-02-07 16:15:10 +0100

    first init

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlin.math.max
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
